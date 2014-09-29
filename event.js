fca4f2e4bf437d9b4649341252964351020507b6
angelinoti <ti-ak@live.fr>
2014-09-30 01:49:04 +0200

    ajout du code html et javascript

---
+++ event.js
@@ -0,0 +1,8 @@
+$( "#login" ).click(function() {
+$( '.login' ).toggleClass( "on" );
+$(this).css("border-bottom-color","#cc9900");
+});
+
+$( ".featured" ).click(function() {
+$( '.plus' ).toggleClass( "down" ); 
+});
